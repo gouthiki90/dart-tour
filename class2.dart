0ada1943632c3610b2cf74ce1cc0be890e5698db
somehow <snsdkf6@gmail.com>
2024-09-11 17:45:33 +0900

    24. Enum

--- class2.dart
+++ class2.dart
@@ -1,7 +1,11 @@
+// 타입을 지정하여 파라미터 작성 시 실수를 줄여줄 수 있다.
+// 예를 들어 css의 padding 값 등
+enum Team { red, blue }
+
 class Player {
   String name;
   int xp;
-  String team;
+  Team team;
 
   // 생성자
   // class에서 이미 타입을 지정해뒀기 때문에 생성자에서 타입을 다시 지정할 필요가 없다.
@@ -19,17 +23,17 @@ class Player {
 }
 
 void main() {
-  var nico = Player(name: 'nico', xp: 1200, team: 'red');
+  var nico = Player(name: 'nico', xp: 1200, team: Team.blue);
   // 예시
-  var nico2 = Player(name: 'nico', xp: 1200, team: 'red')
+  var nico2 = Player(name: 'nico', xp: 1200, team: Team.red)
     ..name = 'las'
     ..xp = 12000
-    ..team = 'blue';
+    ..team = Team.blue;
 
   var potat = nico
     // 해당 오퍼레이터로 파라미터값을 바꿀 수 있음
     ..name = 'las'
     ..xp = 12000
-    ..team = 'blue'
+    ..team = Team.red
     ..sayHello();
 }
