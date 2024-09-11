c837f8b295c7078436a7466744e58220c8772bc5
somehow <snsdkf6@gmail.com>
2024-09-11 17:30:30 +0900

    22. Named Constructors

--- class.dart
+++ class.dart
@@ -1,11 +1,11 @@
 class Player {
   final String name; // 나중에 값을 받을 거라는 late
-  int xp;
+  int xp, age; // 함께 지정 가능
   String team;
-  int age;
 
   // 생성자
   // class에서 이미 타입을 지정해뒀기 때문에 생성자에서 타입을 다시 지정할 필요가 없다.
+  // named constructor
   Player(
       {required this.name,
       required this.xp,
@@ -16,6 +16,24 @@ class Player {
   //   this.xp = xp;
   // }
 
+// 임의로 지정한 파라미터 값을 지정값으로 초기화하는 생성자
+// 콜론을 통해서 초기화를 하겠다고 선언함
+// 네임드 생성자
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  // 포지셔널 생성자
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
   void sayHello() {
     print("Hi my name is $name"); // this.name을 쓸 필요가 없음
     // 함수 안에서 생성된 name이라면 필요함.
@@ -25,18 +43,14 @@ class Player {
 void main() {
   // class의 instance 생성
   // flutter에선 해당 방식이 더 많다.
-  var player = Player(
+  var player = Player.createBluePlayer(
     name: 'nico',
-    xp: 1200,
-    team: 'blue',
     age: 21,
   );
   player.sayHello();
-  var player2 = Player(
-    name: 'lynn',
-    xp: 2500,
-    team: 'red',
-    age: 21,
+  var player2 = Player.createRedPlayer(
+    'lynn',
+    21,
   );
   player2.sayHello();
   // player.name = 'song'; final로 지정했기 때문에 오류가 생긴다.
