4568f0ca748713baaeea533a8a72a9265892ea5e
somehow <snsdkf6@gmail.com>
2024-09-11 17:21:55 +0900

    21 Named Constructor Parameter

--- class.dart
+++ class.dart
@@ -1,10 +1,16 @@
 class Player {
   final String name; // 나중에 값을 받을 거라는 late
   int xp;
+  String team;
+  int age;
 
   // 생성자
   // class에서 이미 타입을 지정해뒀기 때문에 생성자에서 타입을 다시 지정할 필요가 없다.
-  Player(this.name, this.xp);
+  Player(
+      {required this.name,
+      required this.xp,
+      required this.team,
+      required this.age});
   // Player(String name, int xp) {
   //   this.name = name;
   //   this.xp = xp;
@@ -18,10 +24,24 @@ class Player {
 
 void main() {
   // class의 instance 생성
-  var player = Player("nico", 1500);
+  // flutter에선 해당 방식이 더 많다.
+  var player = Player(
+    name: 'nico',
+    xp: 1200,
+    team: 'blue',
+    age: 21,
+  );
   player.sayHello();
-  var player2 = Player("fine", 1600);
+  var player2 = Player(
+    name: 'lynn',
+    xp: 2500,
+    team: 'red',
+    age: 21,
+  );
   player2.sayHello();
   // player.name = 'song'; final로 지정했기 때문에 오류가 생긴다.
   // print(player2.name);
+
+  // 너무 많은 positional argument가 있으면 각 가지고 있는 파라미터가 무엇인지 알기 어렵다.
+  // 따라서 named constructor parameter를 사용한다.
 }
